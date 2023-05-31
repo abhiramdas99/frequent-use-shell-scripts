7222572b9ec703aee42fe7e3bef4be90e1bcf8e8
Abhi <abhiramdas99@gmail.com>
2023-06-01 02:59:00 +0530

    Create linux_rabbitmq_ready.sh
    rabbitmq

---
+++ linux_rabbitmq_ready.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+yum install socat -y
+yum install erlang -y
+yum install wget -y
+wget https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.10/rabbitmq-server-3.6.10-1.el7.noarch.rpm
+rpm --import https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
+yum update
+rpm -Uvh rabbitmq-server-3.6.10-1.el7.noarch.rpm
+systemctl start rabbitmq-server
+systemctl enable rabbitmq-server
+systemctl status rabbitmq-server
+echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config
+rabbitmqctl add_user rabbit bunny
+rabbitmqctl set_user_tags rabbit administrator
+systemctl restart rabbitmq-server
