ccb35e56690eea2c84a8c2b28310307d641c20b6
Abhi <abhiramdas99@gmail.com>
2023-06-04 12:54:07 +0530

    Update ubuntu_lamp_installation.sh

--- ubuntu_lamp_installation.sh
+++ ubuntu_lamp_installation.sh
@@ -1,3 +1,22 @@
+
+##############################################################
+# -- PLEASE READ SCRIPT'S INSTRUCTION BEFORE EXEECUTE IT ----
+# -----------------------------------------------------------
+# * This script is completely install - apache + php + mysql + php my admin
+# 1) The script is compatible for ubuntu OS
+# 2) if its hang during running please use the following 
+#    command in anather terminal and replace the secret 
+#    password -
+#     sudo pkill -f mysql_secure_installation ;
+#     sudo mysql ;
+#     ALTER USER 'root'@'localhost' IDENTIFIED WITH 
+#     mysql_native_password by 'my-secret-password';  
+#     2 is also important for phpmyadmin login 
+# 3) For any aupdate and suggestion in script pleases email to 
+#    abhiramdas99@gmail.com 
+###############################################################
+
+
 #!/bin/bash
 
 # Update package lists
@@ -34,9 +53,3 @@ sudo systemctl restart apache2
 php --version
 mysql --version
 
-################################################
-# ON PASSWORD RELATED IISSUE  RUN THE FOLLWOING COMMAND
-# sudo pkill -f mysql_secure_installation ;
-# sudo mysql ;
-# ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by 'my-secret-password';
-################################################
