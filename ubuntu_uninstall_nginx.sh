30eee019768743746e08f5994006d30f48678c76
Abhi <abhiramdas99@gmail.com>
2023-06-09 01:25:40 +0530

    Update ubuntu_uninstall_nginx.sh

--- ubuntu_uninstall_nginx.sh
+++ ubuntu_uninstall_nginx.sh
@@ -32,3 +32,10 @@ sudo apt-get remove nms-sm
 # uninstall NGINX Agent
 sudo systemctl stop nginx-agent
 sudo apt-get remove nginx-agent
+
+# Delete NGINX directory in all location 
+for i in $(find / -type d -name nginx); do
+        rm -r $i
+done
+
+
